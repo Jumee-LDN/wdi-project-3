7a5f112f8a39fc3d590c5097012de40b3a9107d7
indiaderrick <indiaderrick@gmail.com>
2018-11-16 13:40:16 +0000

    Started set up of models

---
+++ models/exhibition.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const exhibitionSchema = mongoose.Schema({
+  name: String,
+  dates: String,
+  rating: Number,
+  description: String,
+  image: String,
+  gallery: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Gallery'
+  },
+  comments: [
+    {
+      text: String,
+      commentAuthor: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    }
+  ]
+});
+
+const exhibitionModel = mongoose.model('Magazine', exhibitionSchema);
+
+module.exports = exhibitionModel;
