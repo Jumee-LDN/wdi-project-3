334973b9e69c3e7ed24c7f33e888a3f60ef4b1b7
indiaderrick <indiaderrick@gmail.com>
2018-11-19 12:43:22 +0000

    index route working

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
 import 'bulma';
+import Router from './config/router';
 
-function Router($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/'
-    });
-  $urlRouterProvider.otherwise('/');
-}
+// function Router($stateProvider, $urlRouterProvider) {
+//   $stateProvider
+//     .state('home', {
+//       url: '/'
+//     });
+//   $urlRouterProvider.otherwise('/');
+// }
 
 angular.module('exhibition', ['ui.router'])
   .config(Router);
--- src/controllers/exhibitions/exhibitionIndexCtrl.js
+++ src/controllers/exhibitions/exhibitionIndexCtrl.js
@@ -7,9 +7,8 @@ function exhibitionIndexCtrl($scope, $http) {
     $scope.filteredExhibitions = $scope.allExhibitions;
   });
   $scope.handleFilterSubmit = function (){
-    $scope.filteredExhibitions = $scope.allExhibitions.filter(exhibition => exhibition.name.toLocaleLowerCase().includes($scope.searchTerm.toLocaleLowerCase()));
+    $scope.filteredExhibitions = $scope.allExhibitions.filter(exhibition => exhibition.name.toLowerCase().includes($scope.searchTerm.toLowerCase()));
   };
 }
-
 
 export default exhibitionIndexCtrl;
