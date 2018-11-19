86f587596017a4157f62d0edfcadddb74215915f
indiaderrick <indiaderrick@gmail.com>
2018-11-19 18:01:34 +0000

    attempted to fix exhibition create

--- src/config/router.js
+++ src/config/router.js
@@ -58,7 +58,7 @@ function Router($stateProvider) {
     })
     .state('exhibitionNew', {
       templateUrl: './views/exhibitions/exhibitionNew.html',
-      url: '/galleries/:galleryId/new',
+      url: '/galleries/:id/new',
       controller: exhibitionNewCtrl
     });
 }
