97f26825de66f706fa72fe4a199035ff2796b0f6
indiaderrick <indiaderrick@gmail.com>
2018-11-20 17:50:12 +0000

    home fullscreen

--- src/controllers/mainCtrl.js
+++ src/controllers/mainCtrl.js
@@ -1,6 +1,6 @@
 // This controller is global.
 // Things put on $scope here are available everywhere!
-function mainCtrl($scope, $auth, $state) {
+function mainCtrl($scope, $auth, $state, $transitions) {
   $scope.isAuthenticated = function() {
     if ($auth.isAuthenticated()) {
       $scope.username = $auth.getPayload().username;
@@ -15,6 +15,10 @@ function mainCtrl($scope, $auth, $state) {
         console.log('There was an error', err);
       });
   };
+
+  $transitions.onSuccess({}, () => {
+    $scope.isHomepage = $state.$current.name === 'home';
+  });
 }
 
 export default mainCtrl;
